85651fee8e9ec49e74e0ebbb90aa1fe13091527c
afrin100 <hossainafrin388@gmail.com>
2022-10-29 09:11:54 +0600

    added

---
+++ src/Componenets/SignUp/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SignUp.css'
+const Signup = () => {
+    return (
+        <div>
+            <h2>SignUp</h2>
+        </div>
+    );
+};
+
+export default Signup;
